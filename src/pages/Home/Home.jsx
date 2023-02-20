fa8bb878d61fc194c7317774db5337081cda1ef6
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-02-20 17:32:38 -0600

    Setting up the sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import RoundButton from '../../components/RoundButton/RoundButton';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import './Home.css';
 const Home = () => {
 	return (
 		<div>
 			<p>Blanco Media ATL</p>
 			<RoundButton />
+			<Sidebar />
 		</div>
 	);
 };
